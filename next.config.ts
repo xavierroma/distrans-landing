6462c5816e4041a8eba73f478207388a71f5a0fb
Xavier Roma <x.roma@pm.me>
2025-02-16 19:40:50 +0000

    reverse proxy

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,19 @@ const nextConfig: NextConfig = {
         basePath: false
       }
     ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*'
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*'
+      }
+    ];
   }
 };
 
-export default nextConfig;
+export default nextConfig;  
