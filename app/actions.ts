255d5fc33ee27d3219c4bf94ac3a4e33842aa8c5
Xavier Roma <x.roma@pm.me>
2025-02-16 19:29:27 +0000

    posthog actions event

--- app/actions.ts
+++ app/actions.ts
@@ -1,9 +1,17 @@
 'use server'
 
-import posthog from "posthog-js"
+import { PostHog } from "posthog-node";
+
+const posthog = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+})
 
 export async function joinWaitlist(formData: FormData) {
   const email = formData.get('email')
   console.log('Joined waitlist:', email)
-  posthog.capture('joined_waitlist', { email })
+  posthog.capture({
+    distinctId: email as string,
+    event: 'joined_waitlist',
+    properties: { email },
+  });
 }
